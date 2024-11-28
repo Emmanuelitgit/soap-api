ba959a7f2e1ea0f0fd50fa9b59e6529b8024a7e1
Emmanuelitgit <eyidana001@st.ug.edu.gh>
2024-11-28 23:30:48 +0000

    client

---
+++ src/main/java/com/soap/client/SoapClientConfig.java
@@ -0,0 +1,17 @@
+package com.soap.client;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapClientConfig {
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // Set the package where your generated JAXB classes (from WSDL) are located
+        marshaller.setContextPath("com.api.spring.soap");
+        return marshaller;
+    }
+}
